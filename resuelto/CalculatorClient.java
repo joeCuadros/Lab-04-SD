b01c7dfb44c182273f1f1518bd81646075dd3575
joeCuadros <jcuadrosam@unsa.edu.pe>
2025-05-10 12:44:07 -0500

    correcion y evitar *.class

--- resuelto/CalculatorClient.java
+++ resuelto/CalculatorClient.java
@@ -12,6 +12,7 @@ public class CalculatorClient {
             System.out.println( "The substraction of "+num1 +" and "+num2 +" is: "+ c.sub(num1, num2) );
             System.out.println( "The addition of "+num1 +" and "+ num2 +"is: "+c.add(num1, num2) );
             System.out.println( "The multiplication of "+num1 +" and "+num2 +" is: "+c.mul(num1, num2) );
+            System.out.println( "The division of "+num1 +" and "+ num2 +"is: "+c.div(num1, num2) );
         } catch (MalformedURLException murle) {
             System.out.println();
             System.out.println("MalformedURLException");
